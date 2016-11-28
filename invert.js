25fe848af776bfb06999ab453b4d654eb2a6eb36
pooler22 <rudypawel12@gmail.com>
2016-11-28 18:40:43 +0100

    clean up

--- invert.js
+++ invert.js
@@ -1,7 +1,6 @@
 addEventListener('message', onMessage, false);
 
 function onMessage(evt) {
-
     for (var i = 0; i < evt.data.length; i += 4) {
         evt.data[i] = 255 - evt.data[i];
         evt.data[i + 1] = 255 - evt.data[i + 1];
